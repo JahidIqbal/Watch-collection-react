85930774e8ea15a5a43e5a248823b70f4850fef0
hrid620 <hridishere@gmail.com>
2023-01-15 16:00:21 +0600

    done

--- src/Components/AddProducts/AddProducts.js
+++ src/Components/AddProducts/AddProducts.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import React from "react";
 import { useForm } from "react-hook-form";
 
+//products
 const AddProducts = () => {
   const { register, handleSubmit, reset } = useForm();
 
